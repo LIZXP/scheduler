e969438918e2fb15626b446a37d0ce988a815084
LIZXP <Peter.xli@outlook.com>
2022-09-19 22:50:07 -0600

    add interviewers selector and test the function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,8 @@
 export function getAppointmentsForDay(state, day) {
   let resultArr = [];
-  state.days.map((dayArr) => {
-    if (dayArr.name === day) {
-      dayArr.appointments.forEach((appt) => {
+  state.days.map((dayObj) => {
+    if (dayObj.name === day) {
+      dayObj.appointments.forEach((appt) => {
         resultArr.push(state.appointments[appt]);
       });
     }
@@ -17,4 +17,17 @@ export function getInterview(state, interview) {
     return { student: interview.student, interviewer: interviewerName };
   }
   return null;
+}
+
+export function getInterviewersForDay(state, day) {
+  let interviewerArr = [];
+  state.days.map((dayObj) => {
+    if (dayObj.name === day) {
+      dayObj.interviewers.forEach((interviewer) => {
+        interviewerArr.push(state.interviewers[interviewer]);
+      });
+    }
+    return interviewerArr;
+  });
+  return interviewerArr;
 }
