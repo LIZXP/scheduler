29b891424c384207451314c1b00b534025abb63a
LIZXP <Peter.xli@outlook.com>
2022-10-02 14:04:23 -0600

    test the Form components and add the Error message when no student name or interviwer selected

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,8 @@ export default function Form(props) {
   const { interviewers, onSave, onCancel } = props;
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
   const reset = () => {
     setStudent("");
     setInterviewer(null);
@@ -15,9 +17,19 @@ export default function Form(props) {
     onCancel();
   };
 
-  const confirm = () => {
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    setError("");
     onSave(student, interviewer);
-  };
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -31,7 +43,9 @@ export default function Form(props) {
             onChange={(e) => {
               setStudent(e.target.value);
             }}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={interviewers}
@@ -44,7 +58,7 @@ export default function Form(props) {
           <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={confirm}>
+          <Button confirm onClick={validate}>
             Save
           </Button>
         </section>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
+  InterviewerList.propTypes = { interviewers: PropTypes.array };
   const interviewerArray = props.interviewers.map((person) => {
     return (
       <InterviewerListItem
