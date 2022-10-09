eda25f9d1cdc8e94c63e8e11764d08f032c8cdb4
LIZXP <Peter.xli@outlook.com>
2022-10-09 13:36:55 -0600

    add beforeEach to reset the database to separate the test body and add test to edit appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,11 +1,23 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
+  beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
+  });
+  it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
     cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]").first().click({ force: true });
+    cy.get("[data-testid=student-name-input]").clear().type("Nice Guy");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Nice Guy");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
